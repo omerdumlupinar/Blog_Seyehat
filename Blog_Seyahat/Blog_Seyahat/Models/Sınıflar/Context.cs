7a6cfdc73d3db0b105d6f23d596a0e797f60fdae
Ömer Dumlupınar <72862512+omerdumlupinar@users.noreply.github.com>
2023-03-20 03:33:20 +0300

    Yapılan Değişiklikler -->
    Templates projeye eklendi ve türkçeye çevirildi
    Başlangıç Sayfaları için _Layout oluşturuldu
    Entity Framawork CodeFirst yaklaşımını kullanacağım
    için sınıflarımı ve dbset için context sınıfını oluşturdum
    son olarak migrations ayarları yapıldı ve veritabanımız oluştu.

---
+++ Blog_Seyahat/Blog_Seyahat/Models/Sınıflar/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Blog_Seyahat.Models.Sınıflar
+{
+    public class Context:DbContext
+    {
+        public DbSet<Admin>Admins { get; set; }
+        public DbSet<AdresBlog>AdresBlogs { get; set; }
+        public DbSet<Blog>GetBlogs { get; set; }
+        public DbSet<Hakkimizda>Hakkimizdas { get; set; }
+        public DbSet<iletisim>İletisims { get; set; }
+        public DbSet<Yorumlar>Yorumlars { get; set; }
+    }
+}
