4d99640b541cdca2cdf988e12dd6041547ba22e6
Ömer Dumlupınar <72862512+omerdumlupinar@users.noreply.github.com>
2023-03-21 12:03:00 +0300

    Yapılan Değişiklikler ve Eklemeler -->
    Anasayfa,Hakkımızda,Blog ve bloglara yapılan yorum verileri vertabanından çekildi
    anasayfadadaki verileri düzenli çekmek için partialview kullanılarak
    parçalara ayrıldı ve içerikler başlıklarında belirtildiği gibi çekildi

---
+++ Blog_Seyahat/Blog_Seyahat/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog_Seyahat.Models.Sınıflar;
+
+namespace Blog_Seyahat.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
+        public ActionResult Index()
+        {
+            //var degerler = c.GetBlogs.ToList();
+            by.Deger1 = c.GetBlogs.OrderByDescending(x => x.ID).ToList();
+            //sondan 2 Blogu Çekmek için
+            by.Deger3 = c.GetBlogs.OrderByDescending(x => x.ID).Take(7).ToList();
+            by.Deger2 = c.Yorumlars.OrderByDescending(x => x.ID).Take(7).ToList();
+            return View(by);
+        }
+
+        
+        public ActionResult BlogDetay(int id)
+        {
+            //var degerler = c.GetBlogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.GetBlogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+    }
+}
